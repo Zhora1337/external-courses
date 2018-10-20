b086d326e4aaa5e510dada56216504d2515d45b8
Zhora1337 <balakirev.zhora@yandex.ru>
2018-10-20 12:36:16 +0300

    Calculator

---
+++ src/ex5_js-functions/test.js
@@ -0,0 +1,68 @@
+var Calculator = (function() {
+    var result = 0;
+    var _getResult = function() {
+       console.log(result);
+    }
+    var _add = function(operand){
+        result+=operand;
+    }
+    var _subtract = function(operand){
+        result-=operand;
+    }
+    var _multiply = function(operand){
+        result*=operand;
+    }
+    var _divide = function(operand){
+        result/=operand;
+    }
+    var _reset = function(){
+        result = 0;
+    }
+    return {
+        getResult: function() {
+           _getResult(result);
+           return function(){
+               _getResult(result);
+           }
+        },
+        add: function(operand){
+            _add(operand,result);
+            return function(){
+                _add(operand,result);
+            }
+        },
+        subtract:function(operand){
+            _subtract(operand,result);
+            return function(){
+                _subtract(operand,result);
+            }
+        },
+        multiply:function(operand){
+            _multiply(operand,result);
+            return function(){
+                _multiply(operand,result);
+            }
+        },
+        divide:function(operand){
+            _divide(operand,result);
+            return function(){
+                _divide(operand,result);
+            }
+        },
+        reset:function(){
+            _reset(result);
+        }
+    }
+})();
+
+Calculator.getResult();
+Calculator.add(3);
+Calculator.getResult();
+Calculator.subtract(1);
+Calculator.getResult();
+Calculator.multiply(6);
+Calculator.getResult();
+Calculator.divide(3);
+Calculator.getResult();
+Calculator.reset();
+Calculator.getResult();
